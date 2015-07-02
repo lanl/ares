445990edb8a55b184f44ebaae0b97a4bf790ee59
Izzy Cecil <lorr.cecil@gmail.com>
2015-07-02 10:22:07 -0600

    CHANGE: AST setup to be a little sane.

--- include/AST.h
+++ include/AST.h
@@ -21,67 +21,146 @@
 #include <string>
 #include <vector>
 
+enum NodeType {
+  kNum,
+  kVar,
+  kBin,
+  kCall,
+  kProto,
+  kFunc,
+};
+
 enum BinOp {
   kAdd,
   kSub,
   kMul,
-  kDiv
+  kDiv,
 };
 
-class Expr {
-public:
-  virtual ~Expr();
+////////////////////////////////////////////////////////////////
+// Root node types
+////////////////////////////////////////////////////////////////
+// Root Type
+struct AST {
+  AST(NodeType type) : type(type) {};
+  virtual ~AST() {};
+  virtual std::string string() = 0;
+  NodeType type;
+};
+// Root Expression Type
+struct Expr    : AST {
+  Expr(NodeType type) : AST(type) {};
+  virtual ~Expr() {};
+};
+// Root Statement Type
+struct Stat : AST {
+  Stat(NodeType type) : AST(type) {};
+  virtual ~Stat() {};
 };
 
+////////////////////////////////////////////////////////////////
+// Expressions
+////////////////////////////////////////////////////////////////
+struct NumExpr : Expr {
+  NumExpr(double val) : Expr(kNum), val(val) {};
 
-class NumExpr : public Expr {
-private:
+  std::string string() {
+    std::stringstream sstm;
+    sstm << "NumExpr( " << val << ")";
+    return sstm.str();
+  };
+
   double val;
-public:
-  NumExpr(double val) : val(val) {}
 };
 
+struct NameExpr : Expr {
+  NameExpr(const std::string &name) : Expr(kVar), name(name) {};
 
-class VarExpr : public Expr {
-private:
+  std::string string() {
+    std::stringstream sstm;
+    sstm << "NameExpr( " << name << ")";
+    return sstm.str();
+  };
+
+
   std::string name;
-public:
-  VarExpr(const std::string &name) : name(name) {}
 };
 
+struct BinExpr : Expr {
+  BinExpr(BinOp op, Expr *lhs, Expr *rhs)
+    : Expr(kBin), op(op), lhs(lhs), rhs(rhs) {};
 
-class BinExpr : public Expr {
-private:
+  ~BinExpr() {delete lhs; delete rhs;};
+
+  std::string string() {
+    std::stringstream sstm;
+    sstm << "BinExpr( ";
+    switch(op) {
+    case kAdd : sstm << "+"; break;
+    case kSub : sstm << "-"; break;
+    case kMul : sstm << "*"; break;
+    case kDiv : sstm << "/"; break;
+    }
+    sstm << ")";
+    return sstm.str();
+  };
+
+
   BinOp op;
   Expr *lhs, *rhs;
-public:
-  BinExpr(BinOp op, Expr *lhs, Expr *rhs) : op(op), lhs(lhs), rhs(rhs) {}
 };
 
+struct CallExpr : Expr {
+  CallExpr(NameExpr* callee, std::vector<Expr*> args)
+    : Expr(kCall), callee(callee), args(args) {};
 
-class CallExpr : public Expr {
-private:
-  std::string callee;
+  ~CallExpr() {
+    delete callee;
+    for(auto arg : args){
+      delete arg;
+    }
+  };
+
+  NameExpr* callee;
   std::vector<Expr*> args;
-public:
-  CallExpr(const std::string &callee, std::vector<Expr*> args)
-    : callee(callee), args(args) {}
 };
 
+struct Proto : AST {
+  Proto(NameExpr* name, const std::vector<NameExpr*> &args)
+    : AST(kProto), name(name), args(args) {};
 
-class Proto {
-private:
-  std::string name;
-  std::vector<std::string> args;
-public:
-  Proto(const std::string &name, const std::vector<std::string> &args)
-    : name(name), args(args) {}
+  ~Proto() {
+    delete name;
+    for(auto arg : args) {
+      delete arg;
+    }
+  };
+
+  std::string string() {
+    std::stringstream sstm;
+    sstm << "Proto()";
+    return sstm.str();
+  };
+
+  NameExpr* name;
+  std::vector<NameExpr*> args;
 };
 
-class Func {
-private:
-  Proto *proto;
-  Expr *body;
-public:
-  Func(Proto *proto, Expr *body) : proto(proto), body(body) {}
+// Right now functions are single expressions. This is just for testing reasons.
+struct Func : AST {
+  Func(Proto* proto, Expr* body) : AST(kFunc), proto(proto), body(body) {};
+
+  ~Func() {
+    delete proto;
+    delete body;
+  }
+
+  std::string string() {
+    std::stringstream sstm;
+    sstm << "Func()";
+    return sstm.str();
+  };
+
+  Proto* proto;
+  Expr* body;
 };
