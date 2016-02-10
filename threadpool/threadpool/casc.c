85d9cf5d5b6ba47fc55aa2c4fd5b6347b220c9a5
Kei Davis <kei@lanl.gov>
2016-02-10 16:34:48 -0700

    threadpool subdir

--- threadpool/threadpool/casc.c
+++ threadpool/threadpool/casc.c
@@ -1,5 +1,5 @@
 #include "casc.h"
 
-bool cas64(mword *ptr, mword expected, mword desired);
-bool cas128(mword2 *ptr, mword2 expected, mword2 desired);
+bool cas64(qword *ptr, qword expected, qword desired);
+bool cas128(dqword *ptr, dqword expected, dqword desired);
 
