1dc80782cd9b758e53b4c446fec133d9e6a988ff
nickm319 <nickm@lanl.gov>
2016-03-18 15:58:28 -0600

    Add reduce test case.

---
+++ test/reduce/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <ares/frontend.h>
+
+using namespace std;
+using namespace ares;
+
+const size_t SIZE = 100;
+
+int main(int argc, char** argv){
+
+  float sum = 0.0;
+
+  for(auto i : ReduceAll(0, SIZE, sum)){
+    sum += 1;
+  }
+
+  return 0;
+}
