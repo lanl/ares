44ca7892a8dda95769e74885b878f47939114838
Kei Davis <kei@lanl.gov>
2016-06-15 14:26:58 -0600

    fix test.cpp

--- runtime/test.cpp
+++ runtime/test.cpp
@@ -3,6 +3,16 @@
 // g++ ArgoPool.o test.o -o test -L../argobots/install/lib/ -labt
 // ./test
 
+/*
+  # g++ supporting c++14
+  export PATH=/home/kei/projects/gcc-5.3.0-install/bin:$PATH
+  # Argobots installation
+  export LD_LIBRARY_PATH=/home/kei/projects/argobots/install/lib/:$LD_LIBRARY_PATH 
+  g++ -c ArgoPool.cpp -std=c++14 -I../argobots/install/include -Wall
+  g++ -c test.cpp -std=c++14 -I../argobots/install/include  -Wall
+  g++ test.o ArgoPool.o -o test -L../argobots/install/lib/ -labt
+ */
+
 
 #include "ArgoPool.h"
 #include <stdio.h>
@@ -18,7 +28,7 @@ void fun(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-  ArgoPool ap(argc, argv);
+  ArgoPool ap(argc, argv, /*numStreams*/ 4);
   for (intptr_t arg = 1; arg != 11; arg++) {
     ArgoPool::AP_push(fun, (void *)(uintptr_t)arg, 0);
   }
